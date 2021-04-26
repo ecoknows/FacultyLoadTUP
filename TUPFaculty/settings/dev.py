7571a5e5341b14e854312207e2769101a40dda3d
ecoknows <jerico.villaraza@gmail.com>
2021-04-26 23:50:09 +0800

    Config

--- TUPFaculty/settings/dev.py
+++ TUPFaculty/settings/dev.py
@@ -4,7 +4,6 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9%*!&6!l97$k@9f8%^if_r=9qw4huqhjyhe=y1v0!tawa($*j%'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
@@ -16,3 +15,10 @@ try:
     from .local import *
 except ImportError:
     pass
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
