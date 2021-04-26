6309773b3a2726f6d9be31a6309c213b6471fc70
ecoknows <jerico.villaraza@gmail.com>
2021-04-26 23:18:58 +0800

    PostGress

--- TUPFaculty/settings/base.py
+++ TUPFaculty/settings/base.py
@@ -93,9 +93,13 @@ WSGI_APPLICATION = 'TUPFaculty.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_title_db',
     }
 }
 
