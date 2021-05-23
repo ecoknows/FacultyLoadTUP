f74771d549ec0d43b97c05a3e9b95c6f07ef9d13
ecoknows <jerico.villaraza@gmail.com>
2021-05-23 10:35:07 +0800

    adding postre

--- TUPFaculty/settings/base.py
+++ TUPFaculty/settings/base.py
@@ -112,18 +112,18 @@ WSGI_APPLICATION = 'TUPFaculty.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'facultyload',
-        'USER': 'renegado',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': 'project_title_db',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'facultyload',
+    #     'USER': 'renegado',
+    #     'PASSWORD': 'admin',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 
